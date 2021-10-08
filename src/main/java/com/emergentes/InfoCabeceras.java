3b94211a8a9225c75ab7843470483a0e0fb0aa59
Erwin003 <espinoamamanie@gmail.com>
2021-10-08 17:18:42 -0400

    Primer commit

---
+++ src/main/java/com/emergentes/InfoCabeceras.java
@@ -0,0 +1,32 @@
+
+package com.emergentes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ErwinEM
+ */
+@WebServlet(name = "InfoCabeceras", urlPatterns = {"/InfoCabeceras"})
+public class InfoCabeceras extends HttpServlet {
+
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            }
+}
---
+++ target/TE_Servlet-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : index
+    Created on : 28 sep. de 2021, 12:03:48
+    Author     : ErwinEM
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1> Servlet</h1>
+        <ul>
+            <li><a href="HolaMundo">HolaMundo</a></li>
+            <li><a href="InfoCabeceras">InfoCabeceras</a></li>
+            <li><a href="InfoRequest">InfoRequest</a></li>
+            <li><a href="LeeParametros">LeeParametros</a></li>
+            <li><a href="LeeParametrosGET">LeeParametrosGET</a></li>
+            <li><a href="UsoCookies">UsoCookie</a>s</li>
+            <li><a href="UsoSession">UsoSession</a></li>
+        </ul>
+    </body>
+</html>
